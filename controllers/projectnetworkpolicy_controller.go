a8f9445462da4b5e6d142439460aa4316224101b
djkormo <djkormo@gmail.com>
2022-04-23 23:05:49 +0200

    changing operator logic for creating/updating networkpolicies part 2

--- controllers/projectnetworkpolicy_controller.go
+++ controllers/projectnetworkpolicy_controller.go
@@ -18,12 +18,13 @@ package controllers
 
 import (
 	"context"
+	"reflect"
+
 	networkingv1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
-	"reflect"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -55,7 +56,7 @@ func (r *ProjectNetworkPolicyReconciler) Reconcile(ctx context.Context, req ctrl
 
 	logger := log.Log.WithValues(req.Namespace, req.NamespacedName)
 
-	logger.Info("Project operator Reconcile method starts for Network Policy...")
+	logger.Info("Project operator Reconcile Network Policy method starts...")
 
 	// fetch the ProjectNetworkPolicy CR instance
 	ProjectNetworkPolicy := &projectv1alpha1.ProjectNetworkPolicy{}
@@ -72,117 +73,119 @@ func (r *ProjectNetworkPolicyReconciler) Reconcile(ctx context.Context, req ctrl
 		return ctrl.Result{}, err
 	}
 
-	// fetch the ProjectNetworkPolicyTemplate CR instance
-	ProjectNetworkPolicyTemplateFound := &projectv1alpha1.ProjectNetworkPolicyTemplateList{}
-	// Listing all ProjectNetworkPolicyTemplates
-	projNetpoltempErr := r.List(ctx, ProjectNetworkPolicyTemplateFound)
-	if projNetpoltempErr != nil {
-		if errors.IsNotFound(projNetpoltempErr) {
-			// Request object not found, could have been deleted after reconcile request.
-			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
-			// Return and don't requeue
-			logger.Info("ProjectNetworkPolicyTemplate not found. Ignoring since object must be deleted")
-			return ctrl.Result{}, nil
+	// Get array of networkpolicie names
+	netpolnames := ProjectNetworkPolicy.Spec.NetworkPolicies
+	logger.Info("List of NetworkPolicy names", "NetworkPolicy.Names", netpolnames)
+
+	// Get namespace for of network policy
+	netpolnamespace := ProjectNetworkPolicy.Spec.ProjectName
+	logger.Info("Namespace for NetworkPolicy", "NetworkPolicy.Namespace", netpolnamespace)
+
+	// TODO
+
+	// Find if network policy exists
+	networkPolicyFound := &networkingv1.NetworkPolicy{}
+	// Find if projetc network policy template exists
+	projectNetworkPolicyTemplateFound := &projectv1alpha1.ProjectNetworkPolicyTemplate{}
+
+	// Iterate through policy names
+	for _, netpolname := range netpolnames {
+
+		logger.Info("Checking NetworkPolicy", "NetworkPolicy.Name", netpolname, "NetworkPolicy.Namespace", netpolnamespace)
+
+		// get network policy template
+		netpoltemp_err := r.Get(ctx, types.NamespacedName{
+			Name:      netpolname,
+			Namespace: ProjectNetworkPolicy.ObjectMeta.Namespace,
+		}, projectNetworkPolicyTemplateFound)
+
+		if netpoltemp_err == nil {
+
+			logger.Info("Network policy spec:", "NetworkPolicy", projectNetworkPolicyTemplateFound.Spec.PolicySpec, "NetworkPolicy.Name", projectNetworkPolicyTemplateFound.Name)
 		}
-		logger.Error(projNetpoltempErr, "Failed to get Project Operator for NetworkPolicyTemplate")
-		return ctrl.Result{}, projNetpoltempErr
-	}
+		// NetworkPolicyTemplate does'not exist
+		if netpoltemp_err != nil && errors.IsNotFound(netpoltemp_err) {
+			logger.Info("Not existing ProjectNetworkPolicyTemplate", "ProjectNetworkPolicyTemplate.Name", netpolname)
+			logger.Info("Skipping creation of NetworkPolicy", "NetworkPolicy.Name", netpolname)
+			continue
+			// skip the loop for netpolname
+		}
 
-	// iterating all network policy instances
-	for _, projectNetworkPolicytemplateItem := range ProjectNetworkPolicyTemplateFound.Items {
-
-		// Fetch the ProjectNetworkPolicyTemplate instance
-		projnetpoltempinstance := &projectv1alpha1.ProjectNetworkPolicyTemplate{}
 		err = r.Get(ctx, types.NamespacedName{
-			Name:      projectNetworkPolicytemplateItem.ObjectMeta.Name,
-			Namespace: projectNetworkPolicytemplateItem.ObjectMeta.Namespace,
-		}, projnetpoltempinstance)
+			Name: netpolname, Namespace: netpolnamespace}, networkPolicyFound)
 
-		//logger.Info("Getting spec from ProjectNetworkPolicyTemplate", "projectNetworkPolicytemplateItem.Name", projectNetworkPolicytemplateItem.Name, "projectNetworkPolicytemplateItem.Namespace", projectNetworkPolicytemplateItem.Namespace)
-		//logger.Info("Template policy spec:", "projectNetworkPolicytemplateItem.spec", projnetpoltempinstance.Spec.PolicySpec)
+		if err != nil && errors.IsNotFound(err) {
 
-		// Get array of networkpolicie names
-		netpolnames := ProjectNetworkPolicy.Spec.NetworkPolicies
-		// Get namespace for of network policy
-		netpolnamespace := ProjectNetworkPolicy.Spec.ProjectName
-		// Find if network policy exists
-		networkPolicyFound := &networkingv1.NetworkPolicy{}
-
-		// Iterate through policy names
-		for _, netpolname := range netpolnames {
-
-			err = r.Get(ctx, types.NamespacedName{
-				Name: netpolname, Namespace: netpolnamespace}, networkPolicyFound)
-			if err != nil && errors.IsNotFound(err) {
-
-				// Define new networkpolicy
-				netpol := r.networkpolicyForProjectApp(ProjectNetworkPolicy, netpolname, projnetpoltempinstance) // networkpolicyForProjectApp() returns a network policy
-				logger.Info("Creating a new NetworkPolicy", "NetworkPolicy.Name", netpolname, "NetworkPolicy.Namespace", netpol.Namespace)
-				err = r.Create(ctx, netpol)
-				//err = nil
-				// in case of error
-				if err != nil {
-					logger.Error(err, "Failed to create new Network Policy", "NetworkPolicy.Name", netpol.Name, "NetworkPolicy.Namespace", netpol.Namespace)
-					return ctrl.Result{}, err
-				}
-				// in case of success
-				logger.Info("NetworkPolicy created", "NetworkPolicy.Name", netpolname, "NetworkPolicy.Namespace", netpol.Namespace)
-				return ctrl.Result{Requeue: true}, nil
-
-			} else if err != nil {
-				logger.Error(err, "Failed to get NetworkPolicy")
-				// Reconcile failed due to error - requeue
+			// Define new networkpolicy
+			netpol := r.networkpolicyForProjectApp(ProjectNetworkPolicy, netpolname, projectNetworkPolicyTemplateFound) // networkpolicyForProjectApp() returns a network policy
+			logger.Info("Creating a new NetworkPolicy", "NetworkPolicy.Name", netpolname, "NetworkPolicy.Namespace", netpol.Namespace)
+			err = r.Create(ctx, netpol)
+			// in case of error
+			if err != nil {
+				logger.Error(err, "Failed to create new NetworkPolicy", "NetworkPolicy.Name", netpol.Name, "NetworkPolicy.Namespace", netpol.Namespace)
 				return ctrl.Result{}, err
 			}
+			// in case of success
+			logger.Info("NetworkPolicy created", "NetworkPolicy.Name", netpolname, "NetworkPolicy.Namespace", netpol.Namespace)
+			return ctrl.Result{Requeue: true}, nil
 
-			netpol := r.networkpolicyForProjectApp(ProjectNetworkPolicy, netpolname, projnetpoltempinstance) // networkpolicyForProjectApp() returns a network policy
-			labels := ProjectNetworkPolicy.GetLabels()
-			annotations := ProjectNetworkPolicy.GetAnnotations()
-			netpolspec := projnetpoltempinstance.Spec.PolicySpec
+		} else if err != nil {
+			logger.Error(err, "Failed to get NetworkPolicy")
+			// Reconcile failed due to error - requeue
+			return ctrl.Result{}, err
+		}
 
-			netpol_unchanged_labels := IsMapSubset(ProjectNetworkPolicy.ObjectMeta.Labels, labels)
-			netpol_unchanged_annotations := IsMapSubset(ProjectNetworkPolicy.ObjectMeta.Annotations, annotations)
-			netpol_unchanged_spec := reflect.DeepEqual(networkPolicyFound.Spec, netpolspec)
-			if !(netpol_unchanged_labels && netpol_unchanged_annotations && netpol_unchanged_spec && false) {
+		// TODO UPDATE LOGIC
+		logger.Info("Update NetworkPolicy", "NetworkPolicy.Name", netpolname, "NetworkPolicy.Namespace", netpolnamespace)
 
-				logger.Info("Updating NetworkPolicy", "NetworkPolicy.Name", netpolname, "NetworkPolicy.Namespace", netpol.Namespace)
+		netpol := r.networkpolicyForProjectApp(ProjectNetworkPolicy, netpolname, projectNetworkPolicyTemplateFound) // networkpolicyForProjectApp() returns a NetworkPolicy
+		labels := ProjectNetworkPolicy.GetLabels()
+		annotations := ProjectNetworkPolicy.GetAnnotations()
+		// TODO not working good
+		netpolspec := projectNetworkPolicyTemplateFound.Spec.PolicySpec
 
-				if !netpol_unchanged_labels {
-					logger.Info("Desired labels ", "Labels:", labels)
-					logger.Info("Actual labels ", "Labels:", ProjectNetworkPolicy.ObjectMeta.Labels)
-				}
+		netpol_unchanged_labels := IsMapSubset(networkPolicyFound.ObjectMeta.Labels, labels)
+		netpol_unchanged_annotations := IsMapSubset(networkPolicyFound.ObjectMeta.Annotations, annotations)
+		netpol_unchanged_spec := reflect.DeepEqual(netpolspec, networkPolicyFound.Spec)
 
-				if !netpol_unchanged_annotations {
+		if !(netpol_unchanged_labels && netpol_unchanged_annotations && netpol_unchanged_spec) {
+			logger.Info("Update NetworkPolicy changed", "NetworkPolicy.Name", netpolname, "NetworkPolicy.Namespace", netpolnamespace)
+			logger.Info("Update NetworkPolicy changed", "NetworkPolicy.Name", netpolname, "NetworkPolicy.netpol_unchanged_labels", netpol_unchanged_labels)
+			logger.Info("Update NetworkPolicy changed", "NetworkPolicy.Name", netpolname, "NetworkPolicy.netpol_unchanged_annotations", netpol_unchanged_annotations)
+			logger.Info("TODO Update NetworkPolicy changed", "NetworkPolicy.Name", netpolname, "NetworkPolicy.netpol_unchanged_spec", netpol_unchanged_spec)
+			if !netpol_unchanged_labels {
+				logger.Info("Desired labels ", "Labels:", labels)
+				logger.Info("Actual labels ", "Labels:", networkPolicyFound.ObjectMeta.Labels)
+			}
 
-					logger.Info("Desired annotations", "Annotations:", annotations)
-					logger.Info("Actual annotations", "Annotations:", ProjectNetworkPolicy.ObjectMeta.Annotations)
-				}
+			if !netpol_unchanged_annotations {
 
-				if !netpol_unchanged_spec {
+				logger.Info("Desired annotations", "Annotations:", annotations)
+				logger.Info("Actual annotations", "Annotations:", networkPolicyFound.ObjectMeta.Annotations)
+			}
+			if !netpol_unchanged_spec {
 
-					logger.Info("Desired spec", "Annotations:", netpolspec)
-					logger.Info("Actual spec", "Annotations:", networkPolicyFound.Spec)
-				}
+				logger.Info("TODO Desired spec", "Specification:", netpolspec)
+				logger.Info("TODO Actual spec", "Specification:", networkPolicyFound.Spec)
+			}
 
-				networkPolicyFound.ObjectMeta.Labels = labels
-				networkPolicyFound.ObjectMeta.Annotations = annotations
-				networkPolicyFound.Spec = netpolspec
+			netpol.ObjectMeta.Labels = labels
+			netpol.ObjectMeta.Annotations = annotations
+			netpol.Spec = netpolspec
+			err = r.Update(ctx, netpol)
+			if err != nil {
+				logger.Error(err, "Failed to update NetworkPolicy", "NetworkPolicy.Name", netpol.Name, "NetworkPolicy.Namespace", netpol.Namespace)
+				return ctrl.Result{}, err
+			}
+		} else {
+			logger.Info("TODO Update NetworkPolicy unchanged", "NetworkPolicy.Name", netpolname, "NetworkPolicy.Namespace", netpolnamespace)
+			return ctrl.Result{Requeue: true}, nil
 
-				err = r.Update(ctx, netpol)
-				// in case of error
-				if err != nil {
-					logger.Error(err, "Failed to Update NetworkPolicy", "NetworkPolicy.Name", netpol.Name, "NetworkPolicy.Namespace", netpol.Namespace)
-					return ctrl.Result{}, err
-				}
-				// in case of success
-				logger.Info("NetworkPolicy updated", "NetworkPolicy.Name", netpolname, "NetworkPolicy.Namespace", netpol.Namespace)
-				return ctrl.Result{Requeue: true}, nil
+		}
 
-			} // of networkpolicy Update
+		//return ctrl.Result{Requeue: true}, nil
 
-		} // of for iterate through policy names
-
-	} // of for  iterating all network policy instances
+	} //of or _, netpolname := range netpolnames
 
 	return ctrl.Result{Requeue: true}, nil
 }
