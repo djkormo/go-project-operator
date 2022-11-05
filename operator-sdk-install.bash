db586ac2c359cf4edba3c7fbcf31ade81af19cac
Krzysztof Pudłowski <djkormo@gmail.com>
2022-11-05 20:47:28 +0000

    testing install script 2

--- operator-sdk-install.bash
+++ operator-sdk-install.bash
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+set -u
+
+set -e
+
+KUBERNETES_VERSION=v1.22.10
+GO_VERSION=1.17.8
+OPERATOR_SDK_VERSION=v1.19.x
+
+
+
+
 sudo apt-get update
-
 sudo apt-get install snap
-
-exit 0
 
 
 # uninstall existing golang 
 
 sudo rm -rvf /usr/local/go/
 
-# install go 1.17 
+# install go GO_VERSION
 
 
-VERSION="1.17.8" # go version
-ARCH="amd64" # go archicture
+VERSION=${GO_VERSION} # go version
+ARCH="amd64" # go architecture
 curl -O -L "https://golang.org/dl/go${VERSION}.linux-${ARCH}.tar.gz"
 ls -l
 
@@ -60,15 +68,42 @@ sudo chmod +x kubebuilder && sudo mv kubebuilder /usr/local/bin/kubebuilder
 
 git clone https://github.com/operator-framework/operator-sdk
 cd operator-sdk
-git checkout v1.19.x
+git checkout ${OPERATOR_SDK_VERSION}
 make install
-
 
 cd ~
 
-alias k='kubectl'
-
 rm -fr operator-sdk
+
+# Kubernetes staff 
+
+alias k='kubectl'
+alias kubectx='kubectl config use-context '
+alias kubens='kubectl config set-context --current --namespace '
+
+
+# krew plugins 
+
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+
+#Add the $HOME/.krew/bin directory to your PATH environment variable. To do this, update your .bashrc or .zshrc file and append the following line:
+
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+
+kubectl krew install split-yaml
+kubectl krew install neat
+kubectl krew install prune-unused
+
+kubectl krew list 
 
 echo "Component Versions"
 kustomize version
@@ -76,5 +111,6 @@ kubebuilder version
 operator-sdk version
 helm version 
 
-minikube start -p aged --kubernetes-version=v1.22.10
+minikube start -p aged --kubernetes-version=${KUBERNETES_VERSION}
 
+kubectl get nodes -o wide
